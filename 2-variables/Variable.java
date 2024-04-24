6e5db2339758a8b59e77c6c096bb3aa2efebe18f
Aung Ko Man <aungkoman@gmail.com>
2024-04-24 09:37:30 +0630

    Update Variable.java

--- 2-variables/Variable.java
+++ 2-variables/Variable.java
@@ -36,7 +36,6 @@ public class Variable {
         System.out.println(myBool);
         System.out.println(myText);
 
-
         // variable without value
         float myWeight;
         myWeight = 155.5f;
@@ -47,13 +46,53 @@ public class Variable {
         System.out.println(myWeight);
 
         System.out.println("Current Weight " + myWeight);
+        // don't allow override
+        final int birthYear = 1990;
+        // birthYear = 1995; // it does not allow
+
+        // String concatenation
 
         String firstName = "Cisco";
         String lastName = "Ramon";
         String fullName = firstName + lastName;
         System.out.println("Full Name : " + fullName);
-        
 
+        int x = 3;
+        int y = 5;
+        int z = x + y;
+        System.out.println("z is " + z);
+
+        // multiple variables in one line
+        int a = 5, b = 6, c = 50;
+        int d, e, f;
+        d = e = f = 4;
+
+        /*
+         * The general rules for naming variables are:
+         * 
+         * Names can contain letters, digits, underscores, and dollar signs
+         * Names must begin with a letter
+         * Names should start with a lowercase letter, and cannot contain whitespace
+         * Names can also begin with $ and _ (but we will not use it in this tutorial)
+         * Names are case-sensitive ("myVar" and "myvar" are different variables)
+         * Reserved words (like Java keywords, such as int or boolean) cannot be used as
+         * names
+         * 
+         */
+
+        /*
+         * 
+         * + Addition Adds together two values x + y
+         * - Subtraction Subtracts one value from another x - y
+         * Multiplication Multiplies two values x * y
+         * / Division Divides one value by another x / y
+         * % Modulus Returns the division remainder x % y
+         * ++ Increment Increases the value of a variable by 1 ++x
+         * -- Decrement Decreases the value of a variable by 1 --x
+         */
+
+         // comparison, logical operators
+         
 
         // end line spacing
         System.out.println("");
